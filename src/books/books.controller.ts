1ada2fab853bbed3836ad77a37c6eb99d68ad667
hojunin <dlsghwns@naver.com>
2023-08-05 22:38:36 +0900

    feat: 인증 기능 추가

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -3,18 +3,26 @@ import {
   Controller,
   Delete,
   Get,
+  Headers,
   Param,
   Patch,
   Post,
+  UnauthorizedException,
+  UseGuards,
 } from '@nestjs/common';
 import { Book } from './entities/book.entity';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book-dto';
 import { UpdateBookDto } from './dto/update-book-dto';
+import { ConfigService } from '@nestjs/config';
+import { AuthorizationGuard } from 'src/guards/authorization.guard';
 
 @Controller('books')
 export class BooksController {
-  constructor(private readonly BooksService: BooksService) {}
+  constructor(
+    private readonly BooksService: BooksService,
+    private readonly ConfigService: ConfigService,
+  ) {}
   @Get()
   getAll(): Promise<Book[]> {
     return this.BooksService.findAll();
@@ -26,16 +34,19 @@ export class BooksController {
   }
 
   @Post()
+  @UseGuards(AuthorizationGuard)
   createBook(@Body() createBookData: CreateBookDto) {
     return this.BooksService.create(createBookData);
   }
 
   @Delete('/:id')
+  @UseGuards(AuthorizationGuard)
   deleteBook(@Param('id') bookId: number) {
     return this.BooksService.remove(bookId);
   }
 
   @Patch('/:id')
+  @UseGuards(AuthorizationGuard)
   updateBook(@Param('id') bookId: number, @Body() updateData: UpdateBookDto) {
     return this.BooksService.update(bookId, updateData);
   }
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -3,10 +3,11 @@ import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 import { Book } from './entities/book.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [BooksController],
-  providers: [BooksService],
+  providers: [BooksService, ConfigService],
   imports: [TypeOrmModule.forFeature([Book])],
 })
 export class BooksModule {}
