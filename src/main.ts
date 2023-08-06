3775f16ea2720972a69272d7cc87b956db94afeb
hojunin <dlsghwns@naver.com>
2023-08-06 09:22:10 +0900

    validator 이전 데이터에 로거 추가

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,56 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+const CORS_WHITE_LIST = [
+  'https://www.hojunin.com',
+  'https://hojunin.com',
+  'http://localhost:3000',
+];
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = WinstonModule.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.printf(({ timestamp, level, message }) => {
+        return `${timestamp} [${level}] ${message}`;
+      }),
+    ),
+    transports: [
+      new winston.transports.Console(),
+      // Add more transports as needed
+    ],
+  });
+
+  // Custom ValidationPipe with logging
+  class LoggingValidationPipe extends ValidationPipe {
+    async transform(value, metadata) {
+      // Log the value before validation
+      logger.log(`유효성 검증 전 데이터: ${JSON.stringify(value)}`);
+
+      try {
+        // Call the parent transform method for validation
+        const transformedValue = await super.transform(value, metadata);
+        return transformedValue;
+      } catch (error) {
+        logger.error(`유효성 검증에 실패했습니다. ${JSON.stringify(error)}`);
+        throw error;
+      }
+    }
+  }
+
   app.useGlobalPipes(
-    new ValidationPipe({
+    new LoggingValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
     }),
   );
   app.enableCors({
-    origin: [
-      'https://www.hojunin.com',
-      'https://hojunin.com',
-      'http://localhost:3000',
-    ],
+    origin: CORS_WHITE_LIST,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
